8efbef5522b3f67d391e4f3586e1ec56900afc37
Cameron <deancameron99@gmail.com>
2023-10-20 09:40:56 -0400

    Update Index page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 export default function Home () {
   return (
     <div class="nav">
-
+      <h1>Battery Page 290</h1>
+        <h3 className="span loader">
+        <span class="m">O</span><span class="m">N</span><span class="m">E</span><span class="m">&nbsp;</span>
+        <span class="m">S</span><span class="m">T</span><span class="m">O</span><span class="m">P</span><span class="m">&nbsp;</span>
+        <span class="m">S</span><span class="m">H</span><span class="m">O</span><span class="m">P</span>
+        </h3>
 
     </div>
   )
