0d28a22d4fa0a1f4fe938e07c630c406fcc7660f
Cameron <deancameron99@gmail.com>
2023-10-19 21:34:17 -0400

    Restarting index page

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,13 @@
-import Link from "next/link";
+export default function Home () {
+  return (
+    <div class="nav">
 
 
-
-export default function Home() {
-  return(
-    <>
-    <div class="nav-container" className="items-center">
-      <h1 className="text-center">
-        Battery Service 260 <br/>
-        This is your one stop shop. Contact me for all mobile services.
-      </h1>
-        <main>
-            <div class="">
-              <div class="nav-tab ">
-                  <Link href="battery_replacement">
-                    Battery Replacement
-                  </Link>
-              </div>
-              <div class="nav-tab">
-                  <Link href="oil_change">
-                    Oil Change
-                  </Link>
-              </div>
-            </div>
-          </main>
-        </div>
-    </>
+    </div>
   )
 }
+
+
+
+
+
