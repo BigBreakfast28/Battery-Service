37484958d26115c6c85aa14247ff2d2e58a94430
Cameron <deancameron99@gmail.com>
2023-10-23 22:36:20 -0400

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 export default function Home () {
   return (
-    <div class="nav">
-      <h1>Battery Page 290</h1>
+    <section class="nav">
+      <h1>Battery Service 260</h1>
         <h3 className="span loader">
         <span class="m">O</span><span class="m">N</span><span class="m">E</span><span class="m">&nbsp;</span>
         <span class="m">S</span><span class="m">T</span><span class="m">O</span><span class="m">P</span><span class="m">&nbsp;</span>
         <span class="m">S</span><span class="m">H</span><span class="m">O</span><span class="m">P</span>
         </h3>
 
-    </div>
+          <div class="">
+            <a class="nav-tab" href="battery_replacement">Battery-Replacement</a>
+            <a class="nav-tab" href="oil_change"> Oil Change</a>
+            <a class="nav-tab" href="general_inquiry">Inspection</a>
+          </div>
+    </section>
   )
 }
 
